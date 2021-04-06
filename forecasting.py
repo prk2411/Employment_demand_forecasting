310dc14ba018245c9cb0c441bcef7496e1919384
Priyank Patel <64994402+prk2411@users.noreply.github.com>
2021-04-06 12:01:35 -0400

    Add files via upload

--- forecasting.py
+++ forecasting.py
@@ -8,7 +8,7 @@ Created on Thu Mar 18 22:05:13 2021
 import streamlit as st
 import pickle
 from statsmodels.tsa.statespace.sarimax import SARIMAX
-from analysis_functions import plot_models, industry_preprocess
+from analysis_functions import plot_models, industry_preprocess, plot_forecasted
 
 def forecasting(df):
     st.title('Forecasting Employment in Industries')
@@ -16,5 +16,5 @@ def forecasting(df):
     st.subheader('Select an Industry to forecast')
     industry_list = df['NOC'].tolist()
     industry_name = st.selectbox('',(industry_list))
-    fig = plot_models(df, industry_name)
+    fig = plot_forecasted(industry_name)
     st.plotly_chart(fig, use_container_width=True)
